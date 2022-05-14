4c7b3a271043adbf173c6d3b3c91892438f87a3e
Alejandro Falkowski <alexrfalkowski@gmail.com>
2022-05-14 19:32:37 +0200

    build(deps): update all (#14)

--- server/v1/transport/grpc/grpc.go
+++ server/v1/transport/grpc/grpc.go
@@ -8,7 +8,6 @@ import (
 	"github.com/alexfalkowski/go-service/transport/grpc/metrics/prometheus"
 	"github.com/alexfalkowski/go-service/transport/grpc/trace/opentracing"
 	shttp "github.com/alexfalkowski/go-service/transport/http"
-	"github.com/alexfalkowski/go-service/version"
 	v1 "github.com/alexfalkowski/migrieren/api/migrieren/v1"
 	"github.com/alexfalkowski/migrieren/migrate"
 	"github.com/alexfalkowski/migrieren/migrate/migrator"
@@ -27,7 +26,6 @@ type RegisterParams struct {
 	GRPCConfig    *sgrpc.Config
 	Logger        *zap.Logger
 	Tracer        opentracing.Tracer
-	Version       version.Version
 	Metrics       *prometheus.ClientMetrics
 	MigrateConfig *migrate.Config
 	Migrator      migrator.Migrator
@@ -44,7 +42,7 @@ func Register(params RegisterParams) {
 	params.Lifecycle.Append(fx.Hook{
 		OnStart: func(ctx context.Context) error {
 			conn, _ = sgrpc.NewClient(
-				sgrpc.ClientParams{Context: ctx, Host: fmt.Sprintf("127.0.0.1:%s", params.GRPCConfig.Port), Version: params.Version, Config: params.GRPCConfig},
+				sgrpc.ClientParams{Context: ctx, Host: fmt.Sprintf("127.0.0.1:%s", params.GRPCConfig.Port), Config: params.GRPCConfig},
 				sgrpc.WithClientLogger(params.Logger), sgrpc.WithClientTracer(params.Tracer), sgrpc.WithClientDialOption(grpc.WithBlock()),
 				sgrpc.WithClientMetrics(params.Metrics),
 			)
