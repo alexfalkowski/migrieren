57fe9f69b862f6110e3f19472decb3ddb3b5ecbe
Alejandro Falkowski <alexrfalkowski@gmail.com>
2023-01-15 05:51:17 +0100

    feat(config): add other kinds

--- client/config.go
+++ client/config.go
@@ -6,8 +6,8 @@ import (
 
 // Config for client.
 type Config struct {
-	Host     string        `yaml:"host"`
-	Timeout  time.Duration `yaml:"timeout"`
-	Database string        `yaml:"database"`
-	Version  uint64        `yaml:"version"`
+	Host     string        `yaml:"host" json:"host" toml:"host"`
+	Timeout  time.Duration `yaml:"timeout" json:"timeout" toml:"timeout"`
+	Database string        `yaml:"database" json:"database" toml:"database"`
+	Version  uint64        `yaml:"version" json:"version" toml:"version"`
 }
