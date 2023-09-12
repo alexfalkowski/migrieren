6750dcdebbe074507af959a83cbd78d3c1a7247a
Alejandro Falkowski <alexrfalkowski@gmail.com>
2023-09-12 19:02:14 +0200

    feat(deps): upgrade github.com/alexfalkowski/go-service to v1.88.0

--- cmd/server.go
+++ cmd/server.go
@@ -5,6 +5,7 @@ import (
 	"github.com/alexfalkowski/go-service/marshaller"
 	"github.com/alexfalkowski/go-service/metrics"
 	"github.com/alexfalkowski/go-service/otel"
+	"github.com/alexfalkowski/go-service/runtime"
 	"github.com/alexfalkowski/go-service/transport"
 	"github.com/alexfalkowski/migrieren/config"
 	"github.com/alexfalkowski/migrieren/server/health"
@@ -14,7 +15,7 @@ import (
 
 // ServerOptions for cmd.
 var ServerOptions = []fx.Option{
-	fx.NopLogger, marshaller.Module, otel.Module, Module,
-	config.Module, health.Module, logger.ZapModule,
+	fx.NopLogger, runtime.Module, marshaller.Module, otel.Module,
+	Module, config.Module, health.Module, logger.ZapModule,
 	metrics.PrometheusModule, transport.Module, v1.Module,
 }
