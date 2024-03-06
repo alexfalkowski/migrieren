78c50ff15486273edccbaa15749870156a9c6971
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-03-06 07:13:40 +0100

    feat(deps): update github.com/alexfalkowski/go-service to v1.132.1 (#561)

--- client/v1/config/config.go
+++ client/v1/config/config.go
@@ -1,19 +1,12 @@
 package config
 
 import (
-	"time"
-
-	"github.com/alexfalkowski/go-service/retry"
-	"github.com/alexfalkowski/go-service/security"
+	"github.com/alexfalkowski/go-service/config"
 )
 
 // Config for client.
 type Config struct {
-	Host      string          `yaml:"host" json:"host" toml:"host"`
-	Security  security.Config `yaml:"security" json:"security" toml:"security"`
-	Timeout   time.Duration   `yaml:"timeout" json:"timeout" toml:"timeout"`
-	Retry     retry.Config    `yaml:"retry" json:"retry" toml:"retry"`
-	UserAgent string          `yaml:"user_agent" json:"user_agent" toml:"user_agent"`
-	Database  string          `yaml:"database" json:"database" toml:"database"`
-	Version   uint64          `yaml:"version" json:"version" toml:"version"`
+	Database      string `yaml:"database" json:"database" toml:"database"`
+	Version       uint64 `yaml:"version" json:"version" toml:"version"`
+	config.Client `yaml:",inline" json:",inline" toml:",inline"`
 }
