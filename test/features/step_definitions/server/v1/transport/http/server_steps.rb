914fc3999e9c798e4f95f7f80bbe5615175a10a1
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-03-21 07:25:17 +0100

    fix(deps): update github.com/alexfalkowski/go-service to v1.147.1 (#600)

--- test/features/step_definitions/server/v1/transport/http/server_steps.rb
+++ test/features/step_definitions/server/v1/transport/http/server_steps.rb
@@ -30,7 +30,7 @@ def request_with_http(table)
   auth = service_token(Nonnative.configurations('.config/valid.client.yml'))
   opts = {
     headers: {
-      request_id: SecureRandom.uuid, user_agent: Migrieren.server_config.transport.http.user_agent,
+      request_id: SecureRandom.uuid, user_agent: 'Migrieren-ruby-client/1.0 HTTP/1.0',
       content_type: :json, accept: :json
     }.merge(auth),
     read_timeout: 10, open_timeout: 10
