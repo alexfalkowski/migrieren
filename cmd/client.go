0fc13b26d6106f70ffeed627fc684cf59ae9f884
Alex Falkowski <alexrfalkowski@gmail.com>
2024-04-20 19:42:07 +0200

    fix(deps): upgraded github.com/alexfalkowski/auth to v1.85.9

--- cmd/client.go
+++ cmd/client.go
@@ -2,21 +2,19 @@ package cmd
 
 import (
 	ac "github.com/alexfalkowski/auth/client"
-	"github.com/alexfalkowski/go-service/debug"
 	"github.com/alexfalkowski/go-service/feature"
 	"github.com/alexfalkowski/go-service/runtime"
 	"github.com/alexfalkowski/go-service/telemetry"
 	"github.com/alexfalkowski/go-service/telemetry/metrics"
 	mc "github.com/alexfalkowski/migrieren/client"
 	"github.com/alexfalkowski/migrieren/config"
-	"github.com/alexfalkowski/migrieren/transport"
 	"go.uber.org/fx"
 )
 
 // ClientOptions for cmd.
 var ClientOptions = []fx.Option{
-	fx.NopLogger, runtime.Module, debug.Module, feature.Module,
+	fx.NopLogger, runtime.Module, feature.Module,
 	telemetry.Module, metrics.Module,
-	Module, config.Module, transport.Module,
-	ac.Module, mc.CommandModule,
+	config.Module, ac.Module,
+	mc.CommandModule, Module,
 }
