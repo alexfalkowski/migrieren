27186906e1da58bb36c177e9ee8840e2fd3e55cd
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-05-10 07:45:53 +0200

    feat(deps): upgraded github.com/alexfalkowski/auth to v1.112.0 (#751)

--- client/v1/config/config.go
+++ client/v1/config/config.go
@@ -6,7 +6,7 @@ import (
 
 // Config for client.
 type Config struct {
+	*client.Config `yaml:",inline" json:",inline" toml:",inline"`
 	Database       string `yaml:"database,omitempty" json:"database,omitempty" toml:"database,omitempty"`
 	Version        uint64 `yaml:"version,omitempty" json:"version,omitempty" toml:"version,omitempty"`
-	*client.Config `yaml:",inline" json:",inline" toml:",inline"`
 }
