5896ed4d0ae0628351ede56f53d3181a77a38ade
Alex Falkowski <alexrfalkowski@gmail.com>
2024-06-24 05:30:36 +0200

    feat(server): move token to server

--- server/v1/module.go
+++ server/v1/module.go
@@ -2,9 +2,9 @@ package v1
 
 import (
 	"github.com/alexfalkowski/migrieren/migrate"
+	"github.com/alexfalkowski/migrieren/server/security/token"
 	"github.com/alexfalkowski/migrieren/server/service"
 	"github.com/alexfalkowski/migrieren/server/v1/transport/grpc"
-	"github.com/alexfalkowski/migrieren/server/v1/transport/grpc/security/token"
 	"github.com/alexfalkowski/migrieren/server/v1/transport/http"
 	"go.uber.org/fx"
 )
@@ -13,7 +13,7 @@ import (
 var Module = fx.Options(
 	service.Module,
 	migrate.Module,
-	fx.Provide(token.NewVerifier),
+	token.Module,
 	fx.Provide(grpc.NewServer),
 	fx.Invoke(grpc.Register),
 	fx.Invoke(http.Register),
