afaad1b60c59711120f95df17c3e8030588dc991
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-06-25 08:07:10 +0200

    feat(deps): upgraded github.com/alexfalkowski/go-service to v1.221.0 (#825)

--- server/v1/transport/http/http.go
+++ server/v1/transport/http/http.go
@@ -1,22 +1,11 @@
 package http
 
 import (
-	"github.com/alexfalkowski/go-service/marshaller"
 	"github.com/alexfalkowski/go-service/net/http"
 	"github.com/alexfalkowski/migrieren/server/service"
-	"go.uber.org/fx"
 )
 
 type (
-	// RegisterParams for HTTP.
-	RegisterParams struct {
-		fx.In
-
-		Marshaller *marshaller.Map
-		Mux        http.ServeMux
-		Service    *service.Service
-	}
-
 	// Server for HTTP.
 	Server struct {
 		service *service.Service
@@ -29,13 +18,8 @@ type (
 )
 
 // Register for HTTP.
-func Register(params RegisterParams) error {
-	s := &Server{service: params.Service}
+func Register(service *service.Service) {
+	s := &Server{service: service}
 
-	mh := http.NewHandler[MigrateRequest](params.Mux, params.Marshaller, &migrateErrorer{})
-	if err := mh.Handle("POST", "/v1/migrate", s.Migrate); err != nil {
-		return err
-	}
-
-	return nil
+	http.Handler("POST /v1/migrate", &migrateErrorer{}, s.Migrate)
 }
