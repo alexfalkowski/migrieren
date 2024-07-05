6399a967749634ec0d9cd2100c85ce4e571c20f3
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-07-05 12:08:01 +0200

    feat(deps): upgraded github.com/alexfalkowski/go-service to v1.238.0 (#854)

--- server/v1/transport/http/http.go
+++ server/v1/transport/http/http.go
@@ -3,18 +3,18 @@ package http
 import (
 	"net/http"
 
-	nh "github.com/alexfalkowski/go-service/net/http"
+	"github.com/alexfalkowski/go-service/net/http/rpc"
 	"github.com/alexfalkowski/migrieren/server/migrate"
 )
 
 // Register for HTTP.
 func Register(service *migrate.Migrator) {
-	nh.Handle("/v1/migrate", &migrateHandler{service: service})
+	rpc.Handle("/v1/migrate", &migrateHandler{service: service})
 }
 
 func handleError(err error) error {
 	if migrate.IsNotFound(err) {
-		return nh.Error(http.StatusNotFound, err.Error())
+		return rpc.Error(http.StatusNotFound, err.Error())
 	}
 
 	return err
