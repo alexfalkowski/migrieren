98714e86dcaf3e76b59205f4e4317003254f6281
Alex Falkowski <alexrfalkowski@gmail.com>
2024-08-04 11:34:04 +0200

    feat(deps): upgraded github.com/alexfalkowski/go-service to v1.268.0

--- cmd/migrate.go
+++ cmd/migrate.go
@@ -4,7 +4,6 @@ import (
 	"github.com/alexfalkowski/go-service/compress"
 	"github.com/alexfalkowski/go-service/encoding"
 	"github.com/alexfalkowski/go-service/telemetry"
-	"github.com/alexfalkowski/go-service/telemetry/metrics"
 	mc "github.com/alexfalkowski/migrieren/client"
 	"github.com/alexfalkowski/migrieren/cmd/migrate"
 	"github.com/alexfalkowski/migrieren/config"
@@ -14,6 +13,6 @@ import (
 // MigrateOptions for cmd.
 var MigrateOptions = []fx.Option{
 	compress.Module, encoding.Module,
-	telemetry.Module, metrics.Module,
-	config.Module, mc.Module, migrate.Module, Module,
+	telemetry.Module, config.Module,
+	mc.Module, migrate.Module, Module,
 }
--- cmd/server.go
+++ cmd/server.go
@@ -7,7 +7,6 @@ import (
 	"github.com/alexfalkowski/go-service/feature"
 	"github.com/alexfalkowski/go-service/runtime"
 	"github.com/alexfalkowski/go-service/telemetry"
-	"github.com/alexfalkowski/go-service/telemetry/metrics"
 	"github.com/alexfalkowski/go-service/transport"
 	"github.com/alexfalkowski/migrieren/config"
 	"github.com/alexfalkowski/migrieren/server/health"
@@ -19,7 +18,6 @@ import (
 var ServerOptions = []fx.Option{
 	runtime.Module, debug.Module, feature.Module,
 	compress.Module, encoding.Module,
-	telemetry.Module, metrics.Module,
-	transport.Module, health.Module,
+	telemetry.Module, transport.Module, health.Module,
 	config.Module, v1.Module, Module,
 }
