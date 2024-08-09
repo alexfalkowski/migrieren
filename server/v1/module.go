f0ab49e420862a0880ea1dbd6ccf1ac22c96abf7
Alex Falkowski <alexrfalkowski@gmail.com>
2024-08-09 11:33:31 +0200

    feat(security): use new token

--- server/v1/module.go
+++ server/v1/module.go
@@ -3,7 +3,6 @@ package v1
 import (
 	"github.com/alexfalkowski/migrieren/migrate"
 	sm "github.com/alexfalkowski/migrieren/server/migrate"
-	"github.com/alexfalkowski/migrieren/server/security/token"
 	"github.com/alexfalkowski/migrieren/server/v1/transport/grpc"
 	"github.com/alexfalkowski/migrieren/server/v1/transport/http"
 	"go.uber.org/fx"
@@ -13,7 +12,6 @@ import (
 var Module = fx.Options(
 	sm.Module,
 	migrate.Module,
-	token.Module,
 	fx.Provide(grpc.NewServer),
 	fx.Invoke(grpc.Register),
 	fx.Invoke(http.Register),
