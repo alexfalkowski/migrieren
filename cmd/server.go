4bedf080fcf3b5a68c3808ff2a80007ca7973748
Alex Falkowski <alexrfalkowski@gmail.com>
2025-01-18 09:00:41 +0100

    feat(deps): upgraded github.com/alexfalkowski/go-service to v1.376.0
    https://github.com/alexfalkowski/go-service/pull/1194

--- cmd/server.go
+++ cmd/server.go
@@ -1,12 +1,9 @@
 package cmd
 
 import (
-	"github.com/alexfalkowski/go-service/compress"
 	"github.com/alexfalkowski/go-service/debug"
-	"github.com/alexfalkowski/go-service/encoding"
 	"github.com/alexfalkowski/go-service/feature"
-	"github.com/alexfalkowski/go-service/runtime"
-	"github.com/alexfalkowski/go-service/sync"
+	"github.com/alexfalkowski/go-service/module"
 	"github.com/alexfalkowski/go-service/telemetry"
 	"github.com/alexfalkowski/go-service/transport"
 	v1 "github.com/alexfalkowski/migrieren/api/v1"
@@ -17,8 +14,7 @@ import (
 
 // ServerOptions for cmd.
 var ServerOptions = []fx.Option{
-	sync.Module, compress.Module, encoding.Module,
-	runtime.Module, debug.Module, feature.Module,
+	module.Module, debug.Module, feature.Module,
 	telemetry.Module, transport.Module, health.Module,
 	config.Module, v1.Module, Module,
 }
