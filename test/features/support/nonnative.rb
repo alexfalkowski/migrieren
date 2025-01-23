db25f32666291dce773d3f9b4a1a17649f686a8e
Alex Falkowski <alexrfalkowski@gmail.com>
2025-01-23 08:55:14 +0100

    feat(migrate): no need to check version

--- test/features/support/nonnative.rb
+++ test/features/support/nonnative.rb
@@ -3,3 +3,17 @@
 Nonnative.configure do |config|
   config.load_file('nonnative.yml')
 end
+
+Before('@failure') do
+  service = Nonnative.pool.service_by_name('postgres')
+  service.proxy.close_all
+
+  sleep 1
+end
+
+After('@failure') do
+  service = Nonnative.pool.service_by_name('postgres')
+  service.proxy.reset
+
+  sleep 1
+end
