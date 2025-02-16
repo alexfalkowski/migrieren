b6aeb2016f1aff3447679fd94e9b1e10f0abbb1a
Alex Falkowski <alexrfalkowski@gmail.com>
2025-02-16 20:00:24 +0100

    feat(deps): upgraded github.com/alexfalkowski/go-service to v1.439.0
    https://github.com/alexfalkowski/go-service/pull/1317

--- internal/cmd/server.go
+++ internal/cmd/server.go
@@ -16,8 +16,8 @@ import (
 // RegisterServer for cmd.
 func RegisterServer(command *cmd.Command) {
 	flags := flags.NewFlagSet("server")
+	flags.AddInput("env:MIGRIEREN_CONFIG_FILE")
 
-	command.RegisterInput(flags, "env:MIGRIEREN_CONFIG_FILE")
 	command.AddServer("server", "Start migrieren server", flags,
 		module.Module, debug.Module, feature.Module,
 		telemetry.Module, transport.Module, health.Module,
