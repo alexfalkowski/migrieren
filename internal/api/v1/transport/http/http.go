2884ee74e4591fffe20b571ef1cce1cd6b708de5
Alex Falkowski <alexrfalkowski@gmail.com>
2025-04-11 20:40:10 +0200

    feat(api): use grpc full method name

--- internal/api/v1/transport/http/http.go
+++ internal/api/v1/transport/http/http.go
@@ -2,10 +2,11 @@ package http
 
 import (
 	"github.com/alexfalkowski/go-service/net/http/rpc"
+	v1 "github.com/alexfalkowski/migrieren/api/migrieren/v1"
 	"github.com/alexfalkowski/migrieren/internal/api/v1/transport/grpc"
 )
 
 // Register for HTTP.
 func Register(server *grpc.Server) {
-	rpc.Route("/v1/migrate", server.Migrate)
+	rpc.Route(v1.Service_Migrate_FullMethodName, server.Migrate)
 }
