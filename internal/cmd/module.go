7a04d01c13d7de936cb1c344a2bfac0830bed31b
Alejandro Falkowski <alexrfalkowski@gmail.com>
2025-06-05 11:04:24 +0200

    feat(deps): upgraded github.com/alexfalkowski/go-service/v2 to v2.34.0 (#1442)
    https://github.com/alexfalkowski/go-service/pull/1672

--- internal/cmd/module.go
+++ internal/cmd/module.go
@@ -5,14 +5,12 @@ import (
 	v1 "github.com/alexfalkowski/migrieren/internal/api/v1"
 	"github.com/alexfalkowski/migrieren/internal/config"
 	"github.com/alexfalkowski/migrieren/internal/health"
-	"github.com/alexfalkowski/migrieren/internal/token"
 	"go.uber.org/fx"
 )
 
 // Module for fx.
 var Module = fx.Options(
 	module.Server,
-	token.Module,
 	config.Module,
 	health.Module,
 	v1.Module,
--- internal/token/token.go
+++
@@ -1,8 +0,0 @@
-package token
-
-import "github.com/alexfalkowski/go-service/v2/token"
-
-// NewVerifier for token.
-func NewVerifier(token *token.Token) token.Verifier {
-	return token
-}
