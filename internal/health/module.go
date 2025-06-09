84a1a49e38e8d75c13f3c52128f17be4c62ca13d
Alex Falkowski <alexrfalkowski@gmail.com>
2025-06-09 08:35:59 +0200

    feat(deps): upgraded github.com/alexfalkowski/go-service/v2 to v2.60.0
    https://github.com/alexfalkowski/go-service/pull/1707

--- internal/health/module.go
+++ internal/health/module.go
@@ -1,16 +1,12 @@
 package health
 
-import (
-	"github.com/alexfalkowski/migrieren/internal/health/transport/grpc"
-	"github.com/alexfalkowski/migrieren/internal/health/transport/http"
-	"go.uber.org/fx"
-)
+import "go.uber.org/fx"
 
 // Module for fx.
 var Module = fx.Options(
-	fx.Provide(http.NewHealthObserver),
-	fx.Provide(http.NewLivenessObserver),
-	fx.Provide(http.NewReadinessObserver),
-	fx.Provide(grpc.NewObserver),
-	fx.Provide(NewRegistrations),
+	fx.Provide(registrations),
+	fx.Provide(httpHealthObserver),
+	fx.Provide(httpLivenessObserver),
+	fx.Provide(httpReadinessObserver),
+	fx.Provide(grpcObserver),
 )
