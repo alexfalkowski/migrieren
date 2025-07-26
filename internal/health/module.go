00b716ab858a44d66e1e5e45205b581f3a371144
Alex Falkowski <alexrfalkowski@gmail.com>
2025-07-26 18:48:55 +0200

    feat(deps): update github.com/alexfalkowski/go-health/v2 to v2.3.0

--- internal/health/module.go
+++ internal/health/module.go
@@ -4,9 +4,9 @@ import "github.com/alexfalkowski/go-service/v2/di"
 
 // Module for fx.
 var Module = di.Module(
-	di.Constructor(registrations),
-	di.Constructor(httpHealthObserver),
-	di.Constructor(httpLivenessObserver),
-	di.Constructor(httpReadinessObserver),
-	di.Constructor(grpcObserver),
+	di.Register(register),
+	di.Register(httpHealthObserver),
+	di.Register(httpLivenessObserver),
+	di.Register(httpReadinessObserver),
+	di.Register(grpcObserver),
 )
