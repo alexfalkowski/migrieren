af0e1088c03e530e0ea9eaa613e3713bd8567c50
Alex Falkowski <alexrfalkowski@gmail.com>
2025-11-24 19:10:56 +0100

    feat(strings): use strings.Empty

--- internal/cmd/server.go
+++ internal/cmd/server.go
@@ -1,10 +1,13 @@
 package cmd
 
-import "github.com/alexfalkowski/go-service/v2/cli"
+import (
+	"github.com/alexfalkowski/go-service/v2/cli"
+	"github.com/alexfalkowski/go-service/v2/strings"
+)
 
 // RegisterServer for cmd.
 func RegisterServer(command cli.Commander) {
 	cmd := command.AddServer("server", "Start migrieren server", Module)
 
-	cmd.AddInput("")
+	cmd.AddInput(strings.Empty)
 }
